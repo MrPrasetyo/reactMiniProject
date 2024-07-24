9b5da04695e46b8d3fd3f1701bed41e76059e607
MrPrasetyo <prasetyo1103id@gmail.com>
2024-07-24 12:40:17 +0700

    succesfully fetch API, before put in Spinner loading

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,15 @@ import MainLayout from "./layouts/MainLayout";
 
 import React from "react";
 import HomePage from "./pages/HomePage";
+import JobsPage from "./pages/JobsPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
-      <Route index element={<HomePage />} />{" "}
+      <Route index element={<HomePage />} />
+      <Route path='/jobs' element={<JobsPage />} />
+      <Route path='*' element={<NotFoundPage />} />
     </Route>
   )
 );
